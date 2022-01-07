7daf5f6fda51e43211ce5d893fe0f3212de0deb9
Chase <chase.anderson.000@gmail.com>
2022-01-07 03:03:56 -0600

    added monitor
    passing along 3 post for each characters movement paths, have ideas for mob generation as well as stealth tbd

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
+// I would consider this code practically viable
+// ~ looseleif
 public class MoveTo : MonoBehaviour
 {
     // Start is called before the first frame update
        public Transform goal;
+       public Transform post1;
+       public Transform post2;
+       public Transform post3;
 
        public Transform parentModel;
 
@@ -14,29 +20,61 @@ public class MoveTo : MonoBehaviour
        
        private NavMeshAgent agent;
 
+       // public object made for easy manipulation
+
+      public Transform[] locals;
+
+      int i = 0;
+
        private void Start() {
           
           agent = parentOBJ.GetComponent<NavMeshAgent>();
 
+          locals = new Transform[]{post1,post2,post3};
+
+          // inital object assign
+
        }
        void Update () {
 
+          // moving is a variable used to tell if the navmesh navigation should be used
+
           if(moving){
 
-            agent.enabled = true;
+            //agent.enabled = true;
             agent.destination = goal.position;
 
           } else {
 
-            agent.enabled =false;
-            parentModel.position =  new Vector3(parentModel.position.x + 10*Mathf.Cos(Time.frameCount),
-                                                parentModel.position.y,
-                                                parentModel.position.z + 10*Mathf.Sin(Time.frameCount));
+            //agent.enabled =false;
+
+            agent.destination = locals[i].transform.position;
+
+            float dist = Vector3.Distance(transform.position,locals[i].transform.position);
+
+            if(dist<3){
+
+               i++;
+
+            }
+
+            if(i>2){
+
+               i=0;
+
+            }
+
+            // this is used as a basic path for the enemy to use when not engaged
+
+            // parentModel.position =  new Vector3(parentModel.position.x + Mathf.Cos(Time.frameCount),
+            //                                     parentModel.position.y,
+            //                                     parentModel.position.z + Mathf.Sin(Time.frameCount));
 
           }
           
        }
 
+      // Both of these trigger events are used for pathing purposes
        private void OnTriggerEnter(Collider other) {
           
             if(other.gameObject.tag=="Player"){
