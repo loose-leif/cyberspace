43ed8965e977a9f6beea7b5c9ae3f507003b5038
k1llerb333 <akishawy7@gmail.com>
2022-01-09 00:20:57 -0600

    Add new gun button

--- Assets/Scripts/shoot.cs
+++ Assets/Scripts/shoot.cs
@@ -7,7 +7,9 @@ public class shoot : MonoBehaviour
     // Start is called before the first frame update
     public GameObject Bullet_Emitter;
     public GameObject Bullet;
+    public GameObject bombll;
     public float Bullet_Forward_Force = 100;
+    public float bombll_Forward_Force = 100;
     
     void Start()
     {
@@ -17,7 +19,7 @@ public class shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject Temp_bullet_Handler; // bullet happen
             Temp_bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject; //specifies object and postion to instantiate
@@ -25,13 +27,24 @@ public class shoot : MonoBehaviour
             Rigidbody temp_rb; //access rigidbody from instantiated() bullet
             temp_rb = Temp_bullet_Handler.GetComponent<Rigidbody>();
             temp_rb.AddForce(transform.forward * Bullet_Forward_Force); // bullet force 
-            Destroy(Temp_bullet_Handler, 3.0f);
+            Destroy(Temp_bullet_Handler,3.0f);
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            GameObject Temp_bombll_Handler; // bullet happen
+            Temp_bombll_Handler = Instantiate(bombll, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject; //specifies object and postion to instantiate
+            Temp_bombll_Handler.transform.Rotate(Vector3.left * 90); // rotation fix
+            Rigidbody temp_rb2; //access rigidbody from instantiated() bombll
+            temp_rb2 = Temp_bombll_Handler.GetComponent<Rigidbody>();
+            temp_rb2.AddForce(transform.forward * bombll_Forward_Force); // bombll force 
+            Destroy(Temp_bombll_Handler, 3.0f);
 
 
 
-            
+
 
 
         }
+
     }
 }
