14ffc3a9c92c5310783ad061c21bb7c308679889
k1llerb333 <akishawy7@gmail.com>
2022-01-11 00:46:29 -0600

    Add Ammo, Reloading, Switching Weapons, Weapon slot, and rad knife

---
+++ Assets/Scripts/WeaponSlot.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int currentWeapon = 0; //weapons slot
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int preCurrentWeapon = currentWeapon;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+                currentWeapon = 0;
+            else
+                currentWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (currentWeapon <= 0)
+                currentWeapon = transform.childCount - 1;              
+            else
+                currentWeapon--;
+        }
+        if (preCurrentWeapon != currentWeapon)
+        {
+            SelectWeapon();
+        }
+
+    }
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach(Transform weapon in transform) // enables or disables model
+        {
+            if (i == currentWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
--- Assets/Scripts/shoot.cs
+++ Assets/Scripts/shoot.cs
@@ -10,17 +10,40 @@ public class shoot : MonoBehaviour
     public GameObject bombll;
     public float Bullet_Forward_Force = 100;
     public float bombll_Forward_Force = 100;
-    
+    public int maxAmmo = 10;
+    public float reloadTime = 2f;
+    private bool isreloading = false;
+    private int presentAmmo = -1;
+
     void Start()
-    {
-        
+    { if (presentAmmo == -1)
+            presentAmmo = maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isreloading)
+        { 
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) //reloading...
+            { 
+
+            StartCoroutine(reload());
+            return;
+        }
+        if (presentAmmo <= 0)
+        {
+            
+            StartCoroutine(reload()); //reloading  when 0
+            return;
+            
+        }
         if (Input.GetMouseButtonDown(0))
         {
+            presentAmmo--;
             GameObject Temp_bullet_Handler; // bullet happen
             Temp_bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject; //specifies object and postion to instantiate
             Temp_bullet_Handler.transform.Rotate(Vector3.left * 90); // rotation fix
@@ -31,19 +54,25 @@ public class shoot : MonoBehaviour
         }
         if(Input.GetMouseButtonDown(1))
         {
+            
             GameObject Temp_bombll_Handler; // bullet happen
-            Temp_bombll_Handler = Instantiate(bombll, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject; //specifies object and postion to instantiate
+            Temp_bombll_Handler =  Instantiate(bombll, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject; //specifies object and postion to instantiate
             Temp_bombll_Handler.transform.Rotate(Vector3.left * 90); // rotation fix
             Rigidbody temp_rb2; //access rigidbody from instantiated() bombll
             temp_rb2 = Temp_bombll_Handler.GetComponent<Rigidbody>();
             temp_rb2.AddForce(transform.forward * bombll_Forward_Force); // bombll force 
             Destroy(Temp_bombll_Handler, 3.0f);
 
-
-
-
-
-
+        }
+        IEnumerator reload() // the time it takes to reload and resets presentAmmo
+        {
+            Debug.Log("Reloading...");
+            isreloading = true;
+            yield return new WaitForSeconds(reloadTime); // reloading...
+            presentAmmo = maxAmmo;// reset
+            isreloading = false;
+            Debug.Log("Finished");
+      
         }
 
     }
